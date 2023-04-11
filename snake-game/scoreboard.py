6b2f7b973bafa96002decc9ff916244db1c50559
gorkemtore <gorkemtore1@gmail.com>
2023-04-12 02:05:54 +0300

    snake-game upgraded with high-score property

--- snake-game/scoreboard.py
+++ snake-game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
@@ -8,6 +9,7 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = self.get_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 250)
@@ -15,15 +17,27 @@ class ScoreBoard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score : {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER!", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.set_high_score(self.score)
+        
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
 
+    def get_high_score(self):
+        with open("snake-game/score.txt", "r") as file:
+           content = file.read()
+           return int(content)
 
+    def set_high_score(self, new_high_score):
+        with open("snake-game/score.txt", "w") as file:
+            file.write(str(new_high_score))
