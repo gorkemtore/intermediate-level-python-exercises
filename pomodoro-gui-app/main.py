cb13a3eb64ad799526442a8aa367743d3e568b10
gorkemtore <gorkemtore1@gmail.com>
2023-04-20 14:31:17 +0300

    folder direction updated

--- pomodoro-gui-app/main.py
+++ pomodoro-gui-app/main.py
@@ -79,7 +79,7 @@ label_timer_text = Label(text="Timer", font=(FONT_NAME, 35,"bold"), fg=GREEN, bg
 label_timer_text.grid(row=0, column=1)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0) #highlightthickness ==> border
-tomato_img = PhotoImage(file="pomodoro-gui-app/tomato.png")
+tomato_img = PhotoImage(file="../pomodoro-gui-app/tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100,130, text="00:00", font=(FONT_NAME, 35, "bold"), fill="white")
 canvas.grid(row=1, column=1)
