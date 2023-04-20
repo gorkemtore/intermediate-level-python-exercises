f1a2cd6be0d676a162a9d47a177d266b26bf075b
gorkemtore <gorkemtore1@gmail.com>
2023-04-21 00:25:43 +0300

    password-manager search button added to project

--- password-manager-start/main.py
+++ password-manager-start/main.py
@@ -2,15 +2,19 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
+import json
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
-
     entry_password.delete(0, END)
 
-    #Password Generator Project
+    # Password Generator Project
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -27,67 +31,104 @@ def generate_password():
     random.shuffle(password_list)
 
     password = "".join(password_list)
-    entry_password.insert(0, password) 
+    entry_password.insert(0, password)
     pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-
     website = entry_website.get()
     email = entry_email_uname.get()
     password = entry_password.get()
 
-    is_correct = len(website) > 0 and len(email) > 0 and len(password) > 7
-    if is_correct:
-        is_ok = messagebox.askokcancel(title=f"{website}", 
-                                    message=f"These are the details endered.\nEmail: {email} \nPassword: {password}\nIs it ok to save?")
-        
-        if is_ok:
-            with open(file="./password-manager-start/data.txt", mode="a") as file:
-                file.write(f"{website} | {email} | {password}\n")
-            
-            entry_website.delete(0, END)
-            entry_password.delete(0, END)
-            entry_website.focus()
-    else:
-        messagebox.showwarning(title="Oppps!", message="Please make sure you haven't left any fields empty.")
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
-    
+    if len(website) == 0 or len(password) < 7:
+        messagebox.showwarning(title="Oppps!", message="Please make sure you haven't left any fields empty.")
+    else:
+        is_ok = messagebox.askokcancel(title=f"{website}",
+                                       message=f"These are the details entered.\nEmail: {email} \nPassword: {password}\nIs it ok to save?")
+
+        if is_ok:
+            try:
+                with open(file="../password-manager-start/data.json", mode="r") as file:
+                    # reading old data
+                    data = json.load(file)
+            except FileNotFoundError:
+                with open("../password-manager-start/data.json", "w") as file:
+                    json.dump(new_data, file, indent=4)
+            else:
+                # updating old data with new daha
+                data.update(new_data)
+                with open(file="../password-manager-start/data.json", mode="w") as file:
+                    json.dump(data, file, indent=4)
+            finally:
+                entry_website.delete(0, END)
+                entry_password.delete(0, END)
+                entry_website.focus()
+
+
+
+#---------------------------FIND PASSWORD---------------------------------#
+def find_password():
+    website = entry_website.get()
+    try:
+        with open(file="../password-manager-start/data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No data file found")
+    else:
+        if website in data:
+           email = data[website]["email"]
+           password = data[website]["password"]
+           messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Info", message=f"No details for the {website} exists")
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
- 
+
 window = Tk()
 window.title("Password Generator")
 window.config(padx=20, pady=20)
- 
+
 canvas = Canvas(width=200, height=200)
-tomato_img = PhotoImage(file="password-manager-start/logo.png")
+tomato_img = PhotoImage(file="../password-manager-start/logo.png")
 canvas.create_image(100, 100, image=tomato_img)
 canvas.grid(column=1, row=0)
- 
-label_website=Label(text="Website:")
+
+label_website = Label(text="Website:")
 label_website.grid(column=0, row=1)
- 
-entry_website = Entry()
+
+entry_website = Entry(width=35)
 entry_website.focus()
-entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
- 
+entry_website.grid(column=1, row=1, sticky="EW")
+
 label_email_uname = Label(text="Email/Username:")
 label_email_uname.grid(column=0, row=2)
- 
+
 entry_email_uname = Entry()
 entry_email_uname.insert(0, "gorkem@python.com")
 entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW")
- 
+
 label_password = Label(text="Password:")
 label_password.grid(column=0, row=3)
- 
+
 entry_password = Entry()
 entry_password.grid(column=1, row=3, sticky="EW")
- 
+
 generate_btn = Button(text="Generate Password", command=generate_password)
 generate_btn.grid(column=2, row=3, sticky="EW")
- 
-add_btn = Button(text="Add", width=35, command = save)
+
+add_btn = Button(text="Add", width=35, command=save)
 add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
- 
+
+search_btn = Button(text="Search", width=20, command=find_password)
+search_btn.grid(row=1, column=2)
 mainloop()
